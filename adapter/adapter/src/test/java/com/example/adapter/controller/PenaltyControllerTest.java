1f878c868798b917ecf3cc6cbb268ff6facaed23
pashech <pashech555@gmail.com>
2023-02-27 21:24:50 +0300

    add mockRestServiceServer

--- adapter/adapter/src/test/java/com/example/adapter/controller/PenaltyControllerTest.java
+++ adapter/adapter/src/test/java/com/example/adapter/controller/PenaltyControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.adapter.service.impl;
+package com.example.adapter.controller;
 
 import com.example.adapter.client.ClientHttp;
 import com.example.adapter.model.InfoRequest;
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
@@ -21,12 +22,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.response.DefaultResponseCreator;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
 
 import static java.nio.file.Files.readAllBytes;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.client.ExpectedCount.once;
 import static org.springframework.test.web.client.ExpectedCount.times;
@@ -35,37 +39,34 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static java.nio.file.Paths.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class InfoRequestImplTest {
+@AutoConfigureMockMvc
+class PenaltyControllerTest {
 
     @Autowired
     private RestTemplate restTemplate;
-
     @Autowired
-    private PenaltyClientService testSubject;
-
+    private MockMvc mockMvc;
     private MockRestServiceServer mockServer;
-
 
     @BeforeEach
     public void setUp(){
         mockServer = MockRestServiceServer.bindTo(restTemplate).build();
     }
 
-//    @Test
-//    void sendInfoRequest() throws IOException {
-//        byte[] responseBody = readAllBytes(get("src", "test", "resources", "mock", "response", "information-request.json"));
-//        DefaultResponseCreator responseCreator = withStatus(HttpStatus.OK).body(responseBody).contentType(MediaType.APPLICATION_JSON);
-//        mockServer.expect(times(1), requestTo("http://localhost:8081/get/penalties/"))
-//                .andExpect(method(HttpMethod.GET)).andRespond(responseCreator);
-//
-//        ResponseEntity<List<PenaltyDto>> result = testSubject.getPenalty("ETA123456");
-//        mockServer.verify();
-//    }
-
     @Test
-    void sendAcknowledge() {
+    void getPenaltiesTest() throws Exception {
+        byte[] responseBody = readAllBytes(get("src", "test", "resources", "mock", "response", "information-request.json"));
+        DefaultResponseCreator responseCreator = withStatus(HttpStatus.OK).body(responseBody).contentType(MediaType.APPLICATION_JSON);
+        mockServer.expect(times(1), requestTo("http://localhost:8081/get/penalties/ETA123456"))
+                .andExpect(method(HttpMethod.GET)).andRespond(responseCreator);
+
+
+
     }
 }
